3a119900eb90be1536944ed69df967aed0a38588
Barbara Köroglu <barbarakoroglu@MacBook-Air-von-Barbara.fritz.box>
2022-07-05 07:56:29 +0200

    Initial commit

---
+++ src/test/java/PasswordValTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValTest {
+
+    public static void main(String[] args) {
+    String pass1 = "Xs12tzuvai";
+};
+
+
+    public static boolean checkLength (String pass1) {
+        if (pass1.length()>=8){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static boolean contains(String pass1) {
+
+        char[] digits = pass1.toCharArray();
+
+        for (int numbers=0; numbers<pass1.length();numbers++){
+            if (Character.isDigit(digits[numbers])){
+                return true;
+            }
+        } return false;
+    }
+}
+
+
+
+}
